93881eb1ec86c8d541c1f336b244ea3e97c77302
Ever Mamani <evermamanivicente@gmail.com>
2023-12-03 18:06:05 -0400

    Agregadas clases de entidad para usuarios y roles de usuario
    - Creada la clase UserEntity que representa detalles del usuario como nombre de usuario, email, etc.
    - Implementada UserRoleEntity para gestionar roles de usuario y sus asociaciones.
    - Introducida la clase UserRoleId como clave compuesta para UserRoleEntity.
    - Definidas relaciones y restricciones entre entidades de usuario y roles.

---
+++ src/main/java/com/roles/usermanagement/persistance/entity/UserRoleEntity.java
@@ -0,0 +1,38 @@
+package com.roles.usermanagement.persistance.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Representa la relación entre usuarios y roles en la base de datos.
+ */
+@Getter
+@Setter
+@Entity
+@IdClass(UserRoleId.class)
+@Table(name = "user_role")
+public class UserRoleEntity {
+
+  @Id
+  @Column(nullable = false, length = 50, unique = true)
+  private String username;
+
+  @Id
+  @Column(nullable = false, length = 50, unique = true)
+  private String role;
+
+  @Column(nullable = false, length = 200, unique = true, columnDefinition = "DATETIME", name = "granted_date")
+  private LocalDateTime grantedDate;
+
+  @ManyToOne
+  @JoinColumn(name = "username", insertable = false, updatable = false)
+  private UserEntity user;
+}
